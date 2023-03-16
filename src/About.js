0d38dd9ed24f136a466bc7103ed840727ba9116a
Saumya Herath <saumyapiumi7@gmail.com>
2023-03-16 15:20:14 +0530

    add files text replace

--- src/About.js
+++ src/About.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import './Aboutstyle.css';
-import profPic from './Images/newImage.jpg';
+import React from 'react'
+import './Aboutstyle.css'
+import profPic from './Images/newImage.jpg'
+
 
 const About = () => {
   return (
-      <div className='aboutstyle' id='section2'>
-        <div className='container' data-aos='fade-right'>
-          <div className='profpic' style={{ height: '300px', paddingTop: '40px' }}>
-            <img src={profPic} height='100%' />
+      <div className='aboutstyle' id="section2">
+        <div className="container" data-aos="fade-right">
+          <div className="profpic">
+            <img src={profPic} height="400px" />
           </div>
-          <div className='about-heading'>
-            <h1 id='section2' style={{ top: '20%' }}>
-              About
-            </h1 >
+          <div className="about-heading">
+            <h1 id='section2'>About</h1>
           </div>
-          <p className='about-center'>
-            Hello! I'm Saumya Herath, I am a talented and ambitious girl currently studying BSc (Hons) in Information
-            Technology at the University of Sri Jayewardenepura, Colombo. I have a deep passion for coding, and an interest
-            in new designs, and I like to spend my time experiencing lifestyles in new cultures. Moreover, I'm looking for
-            creative and innovative ideas to expand to improve music, travel and skills. and hopes to make a significant
-            impact in the tech world and beyond.
+          <p className="about-text">
+            Hello! I'm Saumya Herath, I am a talented and ambitious girl currently studying BSc (Hons) in Information Technology at the University of Sri Jayewardenepura, Colombo. I have a deep passion for coding, and an interest in new designs, and I like to spend my time experiencing lifestyles in new cultures. Moreover, I'm looking for creative and innovative ideas to expand to improve music, travel and skills. and hopes to make a significant impact in the tech world and beyond.
           </p>
           <br />
           <p>
@@ -31,11 +26,10 @@ const About = () => {
             <li>Wordpress</li>
           </p>
         </div>
-        <div className='landing-text'>
+        <div className="landing-text">
           <h6></h6>
         </div>
       </div>
-  );
-};
-
+  )
+}
 export default About;
