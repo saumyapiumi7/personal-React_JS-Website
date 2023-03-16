7fdad70a73a6f87941feb54a104eab738ca223e7
Saumya Herath <saumyapiumi7@gmail.com>
2023-03-16 20:18:58 +0530

    add project file

--- src/Project.js
+++ src/Project.js
@@ -17,8 +17,8 @@ const Project = () => {
               <img src={webDevelop} height='250px' alt="" />
             </div>
             <div className="portfolio-description">
-              <h6>Desktop Application</h6>
-              <h2>Covid - 19 Vaccines Management System</h2>
+              <h6 style={{fontStyle: 'italic', fontSize: '50px', fontWeight: 'bold'}} >Desktop Application</h6>
+              <h2 style={{fontWeight: 'bold'}} >Covid - 19 Vaccines Management System</h2>
               <p>
                 A desktop application was created using Python to collect essential details related to getting the COVID-19 vaccine.
 
@@ -33,8 +33,8 @@ const Project = () => {
               data-aos-delay="300"
           >
             <div className="portfolio-description has-margin-right" style={{marginLeft: 20}}>
-              <h6>Mobile Application</h6>
-              <h2>AUDIBLE Audio Book </h2>
+              <h6 style={{fontStyle: 'italic', fontSize: '50px', fontWeight: 'bold'}} >Mobile Application</h6>
+              <h2 style={{fontWeight: 'bold'}} >AUDIBLE Audio Book </h2>
               <p>
                 The audiobook mobile application is a revolutionary tool designed specifically for visually impaired individuals who face challenges when it comes to reading books.
 
