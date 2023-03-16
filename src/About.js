45d7129b1321f7c59d7543768daca50c2029d1a9
Saumya Herath <saumyapiumi7@gmail.com>
2023-03-16 14:58:22 +0530

    add files text replace and size change

--- src/About.js
+++ src/About.js
@@ -7,13 +7,13 @@ const About = () => {
   return (
       <div className='aboutstyle' id="section2">
         <div className="container" data-aos="fade-right">
-          <div className="profpic">
+          <div className="profpic" style={{float: 'right'}}>
             <img src={profPic} height="400px" />
           </div>
           <div className="about-heading">
-            <h1 id='section2'style={{ top: '20%' }}>About</h1>
+            <h1 id='section2' style={{marginTop: '20px'}}>About</h1>
           </div>
-          <p className="about-center">
+          <p className="about-center" style={{marginTop: '10px'}}>
             Hello! I'm Saumya Herath, I am a talented and ambitious girl currently studying BSc (Hons) in Information Technology at the University of Sri Jayewardenepura, Colombo. I have a deep passion for coding, and an interest in new designs, and I like to spend my time experiencing lifestyles in new cultures. Moreover, I'm looking for creative and innovative ideas to expand to improve music, travel and skills. and hopes to make a significant impact in the tech world and beyond.
           </p>
           <br />
@@ -32,4 +32,5 @@ const About = () => {
       </div>
   )
 }
+
 export default About;
