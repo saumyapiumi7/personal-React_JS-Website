16b67298f7d6b1381877f86afc24f70808da725a
Saumya Herath <saumyapiumi7@gmail.com>
2023-03-16 20:41:39 +0530

    add project file

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Aboutstyle.css'
+import React from 'react';
+import './Aboutstyle.css';
 
 const Contact = () => {
     return (
@@ -7,7 +7,7 @@ const Contact = () => {
             <section className='container' data-aos="fade-up">
                 <div className="text-center">
                     <div className='contact-heading'>
-                    <h1>Contact</h1>
+                        <h1>Contact</h1>
                     </div>
                     <div className="contact-form">
                         <form id="contact-form" method="POST">
@@ -20,10 +20,16 @@ const Contact = () => {
                         </form>
 
                     </div>
-
+                    <div className="contact-details">
+                        <h2>Contact Details</h2>
+                        <p>Email: saumyapiumi7@gmail.com</p>
+                        <p>Phone: +94 70 311 3226</p>
+                        <p>Address: 437/1, Kohanegama, Malagane, Wariyapola, Sri Lanka</p>
+                    </div>
                 </div>
             </section>
         </div>
-    )
-}
+    );
+};
+
 export default Contact;
