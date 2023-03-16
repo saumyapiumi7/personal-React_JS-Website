4bfcc4c85bbe85c328d899a8b353aa854f73b72d
Saumya Herath <saumyapiumi7@gmail.com>
2023-03-16 12:21:33 +0530

    add file

--- src/Home.js
+++ src/Home.js
@@ -1,64 +1,54 @@
-import React from 'react'
-import Particles from 'react-particles-js'
-import {makeStyles} from "@material-ui/core"
+import React from 'react';
+import Particles from 'react-particles-js';
+import { makeStyles } from "@material-ui/core";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 
 const useStyles = makeStyles({
     particlesCanva: {
         position: "absolute"
     }
-})
+});
 
-const Home = ()=>{
-
-    const classes = useStyles()
+const Home = () => {
+    const classes = useStyles();
 
     return(
         <div className='homestyle' id="section1">
-            <h1 style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
-                Hi, I'm Saumya Herath
-            </h1>
-
-            < >
             <Particles
-            canvasClassName={classes.particlesCanva}
-            params={{
-                particles: {
-                    number:{
-                        value: 80,
-                        density: {
-                            enable: true,
-                            value_area: 850
-                        }
-
-                    },
-
-                    shape: {
-                        type: "circle",
-                        stroke: {
-                            width: 1
-                        }
-                    },
-
-                    size:{
-                        value: 5,
-                        random: true,
-                        anim: {
-                            enable: true,
-                            speed: 10,
-                            size_min: 0.1,
-                            sync: true
+                canvasClassName={classes.particlesCanva}
+                params={{
+                    particles: {
+                        number:{
+                            value: 80,
+                            density: {
+                                enable: true,
+                                value_area: 850
+                            }
+                        },
+                        shape: {
+                            type: "circle",
+                            stroke: {
+                                width: 1
+                            }
+                        },
+                        size:{
+                            value: 5,
+                            random: true,
+                            anim: {
+                                enable: true,
+                                speed: 10,
+                                size_min: 0.1,
+                                sync: true
+                            }
                         }
                     }
-                }
-            }}/>
-
-            </>
-         </div>
-
+                }}
+            />
+            <h1 style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+                Saumya Herath
+            </h1>
+        </div>
     );
 };
-export default Home;
 
+export default Home;
