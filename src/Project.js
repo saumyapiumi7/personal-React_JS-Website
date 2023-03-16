e9fcb96a58a3ec7862cf7695d70672af9aea4846
Saumya Herath <saumyapiumi7@gmail.com>
2023-03-16 19:32:28 +0530

    add project file

--- src/Project.js
+++ src/Project.js
@@ -1,34 +1,54 @@
-import React from 'react';
+import React from 'react'
+import webDevelop from './Images/web.jpg'
+import Android from './Images/android.png'
+import IOS from './Images/ios.jpg'
 
 const Project = () => {
   return (
       <section className='portfoliostyle' id='section4'>
-        <div className="portfolio-heading" style={{textAlign: 'center'}}>
-          <h1 style={{marginTop: '40px'}}>Projects</h1>
-          <p style={{fontFamily: 'Times New Roman', color: '#fff'}}></p>
-          <div className="portfolio-item" data-aos="fade-right" data-aos-delay="200" style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <div className="portfolio-description" style={{textAlign: 'center'}}>
-              <h3 style={{fontWeight: 'bold', color: '#fff', marginBottom: '10px'}}>Desktop Application</h3>
+        <div className="portfolio-heading">
+          <h1 style={{marginTop: '30px'}}>Projects</h1>
+          <p style={{textAlign: 'justify', fontFamily: 'Times New Roman'}}>
+          </p>
+          <div
+              className="portfolio-item"
+              data-aos="fade-left"
+              data-aos-delay="200"
+          >
+            <div className="portfolio-img has-margin-right" style={{float: 'right'}}>
+              <img src={webDevelop} style={{marginRight: '30px'}} height='250px' alt="" />
+            </div>
+            <div className="portfolio-description" style={{float: 'left'}}>
+              <h3 style={{fontWeight: 'bold',color: '#fff', marginBottom: '10px'}}>Desktop Application</h3>
               <h2>Covid-19 Vaccines Management System</h2>
-              <p style={{color: '#fff'}}>
+              <p>
                 A desktop application was created using Python to collect essential details related to getting the COVID-19 vaccine.
+
                 This application serves as a one-stop platform for users to input their personal information, vaccination status, and other relevant details in a secure and user-friendly manner.
               </p>
+
             </div>
           </div>
-          <div className="portfolio-item" data-aos="fade-left" data-aos-delay="200" style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <div className="portfolio-description" style={{textAlign: 'center'}}>
+          <div
+              className="portfolio-item"
+              data-aos="fade-right"
+              data-aos-delay="300"
+          >
+            <div className="portfolio-description has-margin-right" style={{float: 'left', marginRight: '1cm'}}>
               <h3 style={{fontWeight: 'bold', color: '#fff', marginBottom: '10px'}}>Mobile Application</h3>
               <h2>AUDIBLE Audio Book </h2>
-              <p style={{color: '#fff'}}>
+              <p>
                 The audiobook mobile application is a revolutionary tool designed specifically for visually impaired individuals who face challenges when it comes to reading books.
+
                 With its user-friendly interface and seamless functionality, this application offers a comprehensive solution to the accessibility issues faced by visually impaired individuals.
               </p>
+
             </div>
+
           </div>
+
         </div>
       </section>
   )
-};
-
+}
 export default Project;
