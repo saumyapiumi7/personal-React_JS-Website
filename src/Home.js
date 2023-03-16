4380e1b7b7eab993f8c552f1e44ae4fd010e1029
Saumya Herath <saumyapiumi7@gmail.com>
2023-03-16 12:23:59 +0530

    add file

--- src/Home.js
+++ src/Home.js
@@ -44,9 +44,10 @@ const Home = () => {
                     }
                 }}
             />
-            <h1 style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+            <h1 style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: '#fff'}}>
                 Saumya Herath
             </h1>
+
         </div>
     );
 };
