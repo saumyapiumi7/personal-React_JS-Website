54149272af75d8cd12db1cb96c1cdf972e5045ad
Saumya Herath <saumyapiumi7@gmail.com>
2023-03-16 12:00:32 +0530

    add name again

--- src/Home.js
+++ src/Home.js
@@ -1,62 +1,52 @@
-import React from 'react'
-import Particles from 'react-particles-js'
-import {makeStyles} from "@material-ui/core"
+import React from 'react';
+import Particles from 'react-particles-js';
+import { makeStyles } from "@material-ui/core";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 
 const useStyles = makeStyles({
     particlesCanva: {
         position: "absolute"
     }
-})
+});
 
-const Home = ()=>{
+const Home = () => {
+    const classes = useStyles();
 
-    const classes = useStyles()
-    
     return(
         <div className='homestyle' id="section1">
-            <h1>Hello, I'm Saumya Herath</h1>
-            <>
-            <Particles 
-            canvasClassName={classes.particlesCanva}
-            params={{
-                particles: {
-                    number:{
-                        value: 80,
-                        density: {
-                            enable: true,
-                            value_area: 850
-                        }
-                        
-                    },
-
-                    shape: {
-                        type: "circle",
-                        stroke: {
-                            width: 1
-                        }
-                    },
-
-                    size:{
-                        value: 5,
-                        random: true,
-                        anim: {
-                            enable: true,
-                            speed: 10,
-                            size_min: 0.1,
-                            sync: true
+            <h1>Hello, I'm [Your Name]</h1>
+            <Particles
+                canvasClassName={classes.particlesCanva}
+                params={{
+                    particles: {
+                        number:{
+                            value: 80,
+                            density: {
+                                enable: true,
+                                value_area: 850
+                            }
+                        },
+                        shape: {
+                            type: "circle",
+                            stroke: {
+                                width: 1
+                            }
+                        },
+                        size:{
+                            value: 5,
+                            random: true,
+                            anim: {
+                                enable: true,
+                                speed: 10,
+                                size_min: 0.1,
+                                sync: true
+                            }
                         }
                     }
-                }
-            }}/>
-
-            </>
-         </div>  
-        
+                }}
+            />
+        </div>
     );
 };
-export default Home;
 
-      
+export default Home;
